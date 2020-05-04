c53d38275fac65dfdc49bce748400cd70596ace5
alexandergustafssonflink <50074799+alexandergustafssonflink@users.noreply.github.com>
2020-05-04 19:17:28 +0200

    Rich text fixed

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ import Content from "../components/Content";
 import Image from "../components/Image";
 import Post from "../components/Post";
 import Date from "../components/Date";
-import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const Home = () => {
   const [posts, setPosts] = React.useState(null);
@@ -23,17 +23,16 @@ const Home = () => {
   return (
     posts &&
     posts.map((post, i) => {
-      console.log(post);
-
       const fullDate = post.sys.createdAt;
       const splittedDate = fullDate.split("T");
       const date = splittedDate[0];
+      // const content = post.fields.content.content[0].content[0].value;
 
       return (
         <Post key={i}>
           <Title>{post.fields.title}</Title>
           <Date>Skapad: {date}</Date>
-          <Content>{post.fields.content.content[0].content[0].value}</Content>
+          <Content>{documentToReactComponents(post.fields.content)}</Content>
           <Image>{post.fields.image.fields.file.url}</Image>
         </Post>
       );
